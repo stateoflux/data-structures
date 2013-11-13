567ed5710375de7f4ba103d49c0af9e18c9d172f
Wayne Montague <wayne.montague@gmail.com>
2013-11-13 13:28:59 -0800

    stack specs are all passing.  most of queue specs are passing.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size];
+    size++;
   };
 
   instance.dequeue = function(){
+    var result = storage[0];
+
+    for (var i = 1; i < storage.length; i++) {
+      storage[i - 1] = storage[i];
+    }
+    delete storage[storage.length - 1];
+    if (size > 0)
+    size--;
+    return result; 
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,18 +3,27 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.pop = function(){
+    var result = storage[size - 1];
+    delete storage[size - 1];
+    if (size > 0) {
+      size--;
+    }
+    return result;
   };
 
   instance.size = function(){
+    return size;
   };
 
-  return instance;
+  return instance; 
 };
